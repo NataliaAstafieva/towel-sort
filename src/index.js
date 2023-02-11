78aa2ef0f60ced9b9a0b0e958e34603ba4f1c2ac
Natalia Astafieva <asta.natalia31@gmail.com>
2023-02-11 20:20:42 +0300

    feat: add changes in js file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
+let n = [];
+let arr = [];
+let newArr = [];
+let str = '';
+if (arguments.length == 0) {
   return [];
+}
+for (i = 0; i < matrix.length; i++) {
+  n = matrix[i];
+  if (i % 2 !== 0) {
+    n = n.reverse();
+  }
+arr = arr + n + ',';
+str = arr.slice(0, arr.length - 1);
+newArr = str.split(',');
+}
+return newArr;
 }
